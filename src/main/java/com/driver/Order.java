08c9a9e9876ac014aa925e7c97ef801abf1053f4
Rushi <rushibhopale15@gmail.com>
2023-03-04 00:25:24 +0530

    Completed

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,5 +1,8 @@
 package com.driver;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Order {
 
     private String id;
@@ -9,11 +12,22 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        int hour = Integer.parseInt(deliveryTime.substring(0,2));
+        int min = Integer.parseInt(deliveryTime.substring(3));
+        int time = (hour*60)+min;
+        this.deliveryTime = time;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
 }
